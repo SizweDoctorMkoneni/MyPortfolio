779b6e4a2ad814493eae5f98165e16c712cc1034
SizweDoctorMkoneni <sizwemkoneni@gmail.com>
2025-08-07 22:09:28 +0200

    eighth commit. Toggle navigation menu

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+// Scroll to Top Button Functionality
+// This code adds a smooth scroll to the top of the page when the button is clicked.
+
 const scrollToTopBtn = document.querySelector(".scroll-to-top");
 
 scrollToTopBtn.addEventListener('click', () =>{
     window.scrollTo({top: 0, behavior: 'smooth'});
+});
+
+// Toggle Navigation Menu
+// This code toggles the visibility of the navigation menu when the hamburger icon is clicked.
+document.addEventListener('DOMContentLoaded', () => {
+    const humburgerMenu = document.querySelector(".humburger-menu");
+    const navigation = document.querySelector(".nav-menu");
+
+    humburgerMenu.addEventListener('click', () => {
+        navigation.classList.toggle("active");
+        humburgerMenu.classList.toggle("active");
+    });
 });
