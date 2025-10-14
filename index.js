c8c801dd9e598b55400d3f0ac8c0507c6c599010
SizweDoctorMkoneni <sizwemkoneni@gmail.com>
2025-10-14 07:06:07 +0200

    Tenth commit. Media query for extra small devices.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Scroll to Top Button Functionality
 // This code adds a smooth scroll to the top of the page when the button is clicked.
-
 const scrollToTopBtn = document.querySelector(".scroll-to-top");
 
 scrollToTopBtn.addEventListener('click', () =>{
@@ -13,8 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const humburgerMenu = document.querySelector(".humburger-container");
     const navigation = document.querySelector(".nav-menu");
 
+    // open navigation menu
     humburgerMenu.addEventListener('click', () => {
         humburgerMenu.classList.toggle("active");
         navigation.classList.toggle("active");
+    });
+
+    //close navigation menu when a link is clicked
+    navigation.querySelectorAll('li a').forEach(item => {
+        item.addEventListener('click', () => {
+            humburgerMenu.classList.remove("active");
+            navigation.classList.remove("active");
+        });
     });
 });
