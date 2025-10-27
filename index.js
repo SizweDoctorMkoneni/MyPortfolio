d8b65a2cf867ec6d799dc4f9c5eeb325757a5e70
SizweDoctorMkoneni <sizwemkoneni@gmail.com>
2025-10-27 22:40:10 +0200

    11th commit. Addition of a separate responsive css sytlesheet

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// Scroll to Top Button Functionality
-// This code adds a smooth scroll to the top of the page when the button is clicked.
+/***************************************************
+    Scroll to Top Button Functionality.
+    This code adds a smooth scroll to the top of the
+    page when the button is clicked.
+****************************************************/
 const scrollToTopBtn = document.querySelector(".scroll-to-top");
 
 scrollToTopBtn.addEventListener('click', () =>{
     window.scrollTo({top: 0, behavior: 'smooth'});
 });
 
-// Toggle Navigation Menu
-// This code toggles the visibility of the navigation menu when the hamburger icon is clicked.
+/**************************************************** 
+    Toggle Navigation Menu.
+    This code toggles the visibility of the navigation
+    menu when the hamburger icon is clicked.
+******************************************************/
 document.addEventListener('DOMContentLoaded', () => {
     const humburgerMenu = document.querySelector(".humburger-container");
     const navigation = document.querySelector(".nav-menu");
